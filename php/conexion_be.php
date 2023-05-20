193b6def5927a42e028abf5fcc49630c76a56320
PatricioCloude <eduardoypatricioc@gmail.com>
2023-05-20 00:11:12 -0500

    Agregando conexion a la bd / y validando usuario y correo

---
+++ php/conexion_be.php
@@ -0,0 +1,10 @@
+<?php
+    $conexion = mysqli_connect("localhost", "root", "","login_register_db"); //url, usuario,contraseña, nombre bd
+
+/*    if($conexion)   echo 'Conectado exitosamente a la base de datos';
+    else    echo 'No se ah podido conectar a la base de datos';
+*/
+
+
+
+?>
